2e2c1671de1f900511c8b8e035cefaad4d5e6dba
Tomasz Bak <t.bak@selleo.com>
2013-12-12 22:02:32 +0100

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsEmberDemo::Application.config.secret_key_base = '60ddb53e562fb1b410772542f6d31ca7f00e8c29920f5cf0b1845a9ee2acc1a13964725d902e268fcd14c59dc3dd285e1aa81e5e518753d8de81772553ebcbfe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsEmberDemo::Application.config.session_store :cookie_store, key: '_rails-ember-demo_session'
