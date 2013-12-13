c4f9f9de380eba5e7afbaf3730a967bedd977660
Tomasz Bak <t.bak@selleo.com>
2013-12-13 11:26:38 +0100

    add passport pdf

--- spec/support/paperclip.rb
+++ spec/support/paperclip.rb
@@ -3,3 +3,26 @@ require "paperclip/matchers"
 RSpec.configure do |config|
   config.include Paperclip::Shoulda::Matchers
 end
+
+module Paperclip
+  class Attachment
+    def save
+      @queued_for_delete = []
+      @queued_for_write = {}
+      true
+    end
+
+    private
+    def post_process
+      true
+    end
+  end
+
+  #This is only necessary if you're validating the content-type
+  class ContentTypeDetector
+  private
+     def empty?
+       false
+     end
+  end
+end
